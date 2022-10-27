92f7a97b0ad19e593734ce45a6ae59f3b96b080a
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-27 15:28:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+`use strict`
+
+const shoppingList = [
+    'pane',
+    'latte',
+    'cereali',
+    'verdure',
+    'affettati',
+];
+console.log(shoppingList);
+
+const listContainer = document.getElementById("list");
+let i = 0;
+while( i <= shoppingList.lenght ) {
+console.log(shoppingList[i]);
+i++;
+
+}
+    
+const listItem = document.createElement("li");
+listItem.innerHTML = shoppingList[i];
+/* inserisco l'elemento appena creato nella lista */
+    
+listContainer.append(listItem);
