fab21c4515441ae82eefe4295bb925261260d61f
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-27 16:22:40 +0200

    Inserito getElement dentro while

--- main.js
+++ main.js
@@ -9,15 +9,16 @@ const shoppingList = [
 ];
 console.log(shoppingList);
 
-const listContainer = document.getElementById("list");
-let i = 0;
+let i = -1;
 while( i < shoppingList.length ) {
 console.log(shoppingList[i]);
 i++;
+
 const listItem = document.createElement("li");
 listItem.innerHTML = shoppingList[i];
+
 /* inserisco l'elemento appena creato nella lista */
-    
+const listContainer = document.getElementById("list");
 listContainer.append(listItem);
 }
   
